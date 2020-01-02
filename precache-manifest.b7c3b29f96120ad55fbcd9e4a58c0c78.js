1f2b37a9bcbb596b017118ca21ad7047dac577c3
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-01-02 23:57:57 +0100

    Updates

---
+++ precache-manifest.b7c3b29f96120ad55fbcd9e4a58c0c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae540407e277ed9854313285cc38d4e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e471e7441375dee64b3",
+    "url": "/monsters-rolodex/static/css/main.aff19e3a.chunk.css"
+  },
+  {
+    "revision": "298b71f4a094e38c52a1",
+    "url": "/monsters-rolodex/static/js/2.faa2861c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.faa2861c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e471e7441375dee64b3",
+    "url": "/monsters-rolodex/static/js/main.9dd9dd63.chunk.js"
+  },
+  {
+    "revision": "b7f997f99e8450397d38",
+    "url": "/monsters-rolodex/static/js/runtime-main.90325ef8.js"
+  }
+]);
